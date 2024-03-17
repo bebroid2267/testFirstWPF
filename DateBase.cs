5e19e5d744d760cc6524004b2a0963405b440371
bebroid2267 <148254843+bebroid2267@users.noreply.github.com>
2024-03-17 21:04:30 +0500

    Добавьте файлы проекта.

---
+++ DateBase.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+
+namespace testFirstWPF
+{
+    public static class DateBase
+    {
+        private static readonly string connectionString = @"Data Source = /root/restaurantbot/restaurant.db";
+
+        private async static Task<bool> IfProcessExists(string name)
+        {
+            using (SqliteConnection connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+
+                var command = new SqliteCommand();
+                command.Connection = connection;
+                command.CommandText = $"SELECT COUNT(*) FROM Process WHERE Name LIKE '{name}'";
+
+                int existsProcess = (int)await command.ExecuteScalarAsync();
+
+                if(existsProcess > 0)
+                {
+                    connection.Close();
+                    return true;
+                }
+                else
+                {
+                    connection.Close();
+                    return false;
+                }
+
+
+            }
+
+        }
+
+        public async static Task AddProcess(string name, string startTime)
+        {
+            using (SqliteConnection connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+
+                var command = new SqliteCommand();
+                command.Connection = connection;
+                if (await IfProcessExists(name))
+                {
+                    command.CommandText = $"INSERT INTO Process (Name,  Time) values (@Name, @Time)";
+
+                    command.Parameters.AddWithValue("@Name", name);
+                    command.Parameters.AddWithValue("@Time", startTime);
+
+                    await command.ExecuteNonQueryAsync();
+
+                    connection.Close();
+                }
+                else
+                {
+                    connection.Close();
+                }
+                
+
+
+            }
+
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Data.Sqlite;
+using System.Management;
+
+
+namespace testFirstWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public async Task MonitorProcesess()
+        {
+            ManagementObjectSearcher processes = new ManagementObjectSearcher("SELECT * FROM Win32_Process");
+            if (processes != null)
+            {
+
+                foreach (ManagementObject process in processes.Get())
+                {
+                    await DateBase.AddProcess(process["Name"].ToString(), DateTime.UtcNow.AddHours(3).ToString("T"));
+                    Console.WriteLine(process["Name"]);
+                }
+            }
+        }
+
+        public void StartProcess()
+        {
+
+
+        }
+    }
+}
